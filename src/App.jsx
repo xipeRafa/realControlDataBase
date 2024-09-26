e42fafc2b9fd5dcebae451c0e8746bd3b7f7c242
fer <fer@fers-iMac.local>
2024-09-26 15:31:42 -0700

    koko

--- src/App.jsx
+++ src/App.jsx
@@ -36,12 +36,15 @@ import Col from 'react-bootstrap/Col';
 
 function App() {
 
+  const [DateMS, setDateMS]=useState('')
+
 
   const [items, setItems] = useState([]);
+  const [tecnicoState, setTecnicoState]=useState('')
 
   const itemCollection = query(
       collection(firestoreDB, 'tasksRealControl'),
-      where('asignadoPara', '==', localStorage.getItem('user'))
+      where('asignadoPara', '==', tecnicoState)
   )
 
   const [toggle, setToggle] = useState(true)
@@ -67,312 +70,66 @@ function App() {
   }, [toggle])
 
 
-
-  const updateById = async (id, obj) => {
-
-    if (confirm("Marcar como Servicio Completado")) {
-      obj.completed = true
-      obj.completedTime = Date.now()
-      delete obj.id
-
-      const aDoc = doc(firestoreDB, 'tasksRealControl', id)
-
-      try {
-          await updateDoc(aDoc, obj);
-      } catch (error) {
-          console.error(error);
-      }
-
+  useEffect(()=>{
       setToggle(!toggle)
-    }
-
-  }
+  },[tecnicoState])
 
 
 
+  // const updateById = async (id, obj) => {
 
-  const [show, setShow] = useState(false);
+  //   if (confirm("Marcar como Servicio Completado")) {
+  //     obj.completed = true
+  //     obj.completedTime = Date.now()
+  //     delete obj.id
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  //     const aDoc = doc(firestoreDB, 'tasksRealControl', id)
 
+  //     try {
+  //         await updateDoc(aDoc, obj);
+  //     } catch (error) {
+  //         console.error(error);
+  //     }
 
-  const [usuarioState, setUsuarioState]=useState()
-  const [usuarioClaveState, setClaveState]=useState()
+  //     setToggle(!toggle)
+  //   }
 
-  let admin = 'admin'
-  let uno = 'koko'
-  let dos = 'lima'
-  let tres = 'lona'
-
-
-  const saveToLocalStorage = () => {
-
-    if(localStorage.user === undefined){
-
-      if(usuarioState === 'admin'){
-          if(usuarioClaveState === 'admin'){
-              localStorage.setItem('user', usuarioState)
-              handleClose()
-              setTimeout(()=>{ location.reload() },600)
-          }else{
-              alert('Clave Incorrecta')
-          }
-      }
-
-      if(usuarioState === 'usuario1'){
-          if(usuarioClaveState === 'koko'){
-              localStorage.setItem('user', usuarioState)
-              handleClose()
-              setTimeout(()=>{ location.reload() },1600)
-          }else{
-              alert('Clave Incorrecta')
-          }
-      }
-
-      if(usuarioState === 'usuario2'){
-          if(usuarioClaveState === 'lima'){
-              localStorage.setItem('user', usuarioState)
-              handleClose()
-              setTimeout(()=>{ location.reload() },1600)
-          }else{
-              alert('Clave Incorrecta')
-          }
-      }
-      
-      if(usuarioState === 'usuario3'){
-          if(usuarioClaveState === 'lona'){
-              localStorage.setItem('user', usuarioState)
-              handleClose()
-              setTimeout(()=>{ location.reload() },1600)
-          }else{
-              alert('Clave Incorrecta')
-          }
-      }
-
-    }else{
-        localStorage.removeItem('user')
-        setTimeout(()=>{ location.reload() },1600)
-    }
-
-  }
+  // }
 
 
 
-
-  const[taskState, setTaskState]=useState({
-    asignadoPara:"",
-    comentarios:"",
-    consumibles:"",
-    direccionCliente:"",
-    fechaMeta:"",
-    nombreCliente:"",
-    servicioDescripcion:"",
-    tipoDeServicio:""
-  })
-
-
-
-  const {
-    asignadoPara,
-    comentarios,
-    consumibles,
-    direccionCliente,
-    fechaMeta,
-    nombreCliente,
-    servicioDescripcion,
-    tipoDeServicio
-  } = taskState
-
-
-  const handlerTaskState=({target})=>{
-      const {name, value} = target
-      setTaskState({...taskState, [name]:value})
-  }
-
-
-
-  const postCollection = collection(firestoreDB, 'tasksRealControl');
-
-  const guardar =()=>{
-
-      if (asignadoPara.trim() === '' ||
-          comentarios.trim() === '' ||
-          consumibles.trim() === '' ||
-          direccionCliente.trim() === '' ||
-          fechaMeta.trim() === '' ||
-          nombreCliente.trim() === '' ||
-          servicioDescripcion.trim() === '' ||
-          tipoDeServicio.trim() === '' ){
-              alert('Algun Campo esta Vacio')
-              return
-          }
-
-      if (confirm("Crear Servicio")) {
-          taskState.createdAt = Date.now()
-          taskState.completed = false
-          addDoc(postCollection, taskState)
-          setTaskState({
-              asignadoPara:"",
-              comentarios:"",
-              consumibles:"",
-              direccionCliente:"",
-              fechaMeta:"",
-              nombreCliente:"",
-              servicioDescripcion:"",
-              tipoDeServicio:""
-          })
-      }
-
+  
+    const setDate=(e)=>{
+        let DateToCero = Date.parse(e.target.value.replace('-', '/').replace('-', '/'))
+        setDateMS(DateToCero)
     }
 
 
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
-          {localStorage.user} {localStorage.user ? '➪' : 'Entrar'} 
-      </Button>
 
 
 
-      <Modal show={show} onHide={handleClose}>
-
-          <Modal.Header closeButton>
-              <Modal.Title> {localStorage.user ? localStorage.user : 'Entrar'}</Modal.Title>
-          </Modal.Header>
-
-            {!localStorage.user ? 
-          <Modal.Body>
-
-              <Form.Label>Usuarios:</Form.Label>
-              <Form.Select
-                  value={usuarioState}
-                  name="usuario"
-                  onChange={(e)=>setUsuarioState(e.target.value)}>
-
-                  <option></option>
-                  <option value="admin">Admin</option>
-                  <option value="usuario1">Usuario 1</option>
-                  <option value="usuario2">Usuario 2</option>
-                  <option value="usuario3">Usuario 3</option>
-              </Form.Select>
-
-              <Form.Label>Clave:</Form.Label>
-              <Form.Control type="password" onChange={(e)=>setClaveState(e.target.value)} />
-            
-          </Modal.Body>
-
-          : ''}
-
-          <Modal.Footer>
-              <Button variant="primary" onClick={saveToLocalStorage}>{localStorage.user ? 'Salir ➪' : 'Entrar'}</Button>
-          </Modal.Footer>
-
-      </Modal>
-
-    {localStorage.user === 'admin' ? 
       <Container>
-        <Row>
-          <Col>
+      <hr />
 
-
-
-
-        <Form>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Nombre de Cliente</Form.Label>
-        <Form.Control type="text" name='nombreCliente' value={nombreCliente} onChange={(e)=>handlerTaskState(e)} />
-
-        <Form.Label>Direccion de Cliente</Form.Label>
-        <Form.Control type="text" name='direccionCliente' value={direccionCliente} onChange={(e)=>handlerTaskState(e)}/>
-
-        <Form.Label>Descripción del Servicio</Form.Label>
-        <Form.Control type="text" name='servicioDescripcion' value={servicioDescripcion} onChange={(e)=>handlerTaskState(e)}/>
-
-        <Form.Label>Fecha y Hora</Form.Label>
-        <Form.Control type="text" name='fechaMeta' value={fechaMeta} onChange={(e)=>handlerTaskState(e)}/>
-
-        <Form.Label>Tipo de Servicio</Form.Label>
-        <Form.Select
-              value={tipoDeServicio}
-              name='tipoDeServicio' 
-              onChange={(e)=>handlerTaskState(e)}>
-
-              <option></option>
-              <option value="domestica chica">Domestica Chica</option>
-              <option value="comercial chica">Comercial Chica</option>
-              <option value="industrial chica">Industrial Chica</option>
-
-              <hr />
-              <option value="domestica mediana">Domestica Mediana</option>
-              <option value="comercial mediana">Comercial Mediana</option>
-              <option value="industrial mediana">Industrial Mediana</option>
-
-              <hr />
-              <option value="domestica grande">Domestica Grande</option>
-              <option value="comercial grande">Comercial Grande</option>
-              <option value="industrial grande">Industrial Grande</option>
-        </Form.Select>
-
-        <Form.Label>Consumibles a Utilizar</Form.Label>
-        <Form.Control as="textarea" name='consumibles' value={consumibles} onChange={(e)=>handlerTaskState(e)} />
-
-
-
-
-        <Form.Label>Asignar a:</Form.Label>
-        <Form.Select
-              value={asignadoPara}
-              name='asignadoPara' 
-              onChange={(e)=>handlerTaskState(e)}>
-
-              <option></option>
-              <option value="usuario1">Usuario 1</option>
-              <option value="usuario2">Usuario 2</option>
-              <option value="usuario3">Usuario 3</option>
-        </Form.Select>
-
-      
-
-
-
-
-
-
-
-
-      </Form.Group>
-
-
-
-        <Form.Label>Comentarios</Form.Label>
-        <Form.Control as="textarea"  name='comentarios' value={comentarios} onChange={(e)=>handlerTaskState(e)} />
-
-
-    </Form>
-
-      <Button variant="primary" onClick={guardar}>
-          GUARDAR
-      </Button>
-
-
-
-    </Col>
+        <Row className='d-flex text-center'>
+            <Col><input type='date'  onChange={(e)=>setDate(e)}/> </Col>
+            <Col><Button className={tecnicoState !== 'usuario1' ? 'gray' : 'blue' } onClick={()=>setTecnicoState('usuario1')}>Usuario1</Button> </Col>
+            <Col><Button className={tecnicoState !== 'usuario2' ? 'gray' : 'blue' } onClick={()=>setTecnicoState('usuario2')}>Usuario2</Button> </Col>
+            <Col><Button className={tecnicoState !== 'usuario3' ? 'gray' : 'blue' } onClick={()=>setTecnicoState('usuario3')}>Usuario3</Button> </Col>
         </Row>
-      </Container>
 
-          : 
-
-
-
-      <Container>
         <Row>
           <Col>
-              {items?.slice(0,12).sort((a, b) => b.createdAt - a.createdAt).map((el, i)=>(
+
+              {/*{tecnicoState}*/}
+              {items?.sort((a, b) => b.createdAt - a.createdAt)
+                    .filter(el=>el.createdAt > DateMS)
+                    .filter(el=>el.createdAt < DateMS + 86400000).map((el, i)=>(
                   <div key={i}>
 
                     <hr />
-
                     <p>Cliente: {el.nombreCliente}</p>
                     <p>Direccion: {el.direccionCliente}</p>
                     <p>Servicio: {el.servicioDescripcion}</p>
@@ -382,7 +139,7 @@ function App() {
                     <p>Comentarios: {el.comentarios}</p>
                     <p>Tarea Creada el: {msecToDateNumbers(el.createdAt)}</p>
 
-                    <Button disabled={el.completed} variant="primary" className={el.completed ? '' : 'red'} onClick={()=>updateById(el.id, el)}>
+                    <Button disabled={el.completed} variant="primary" className={el.completed ? '' : 'red'}>
                         {el.completed ? 'Completado' : 'Pendiente'}
                     </Button>
 
@@ -393,10 +150,10 @@ function App() {
                 ))}
           </Col>
         </Row>
+
       </Container>
 
 
-        }
 
 
     </>
